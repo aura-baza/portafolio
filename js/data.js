ec83f9862f335a02bf50607d0d33aaf94d2ff260
aura-baza <aurabaza17@gmail.com>
2023-04-28 16:50:09 -0500

    agregando un nuevo proyecto

--- js/data.js
+++ js/data.js
@@ -1,10 +1,11 @@
 export let projects =[
-    // {id:1, description:"En esta aplicacion es para realizar registros, está centrada en la logica, maquetación y diseño responsivo, ya que no guarda registros; no está conectada a una base de datos. Los registros de pruebas fueron guardados en el localStorage.", name:"Registros", link:"https://aura-baza.github.io/crud_registers/", image:"./images/proyecto1.jpg", category:"app"},
+    {id:1, description:"En esta aplicacion es para realizar registros, está centrada en la logica, maquetación y diseño responsivo, ya que no guarda registros; no está conectada a una base de datos. Los registros de pruebas fueron guardados en el localStorage.", name:"Registros", link:"https://aura-baza.github.io/crud_registers/", image:"./images/proyecto1.jpg", category:"app"},
     {id:2, description:"Esta pagina muestra las peliculas más recientes y las más vistas   ", name:"Movies", link:"https://aura-baza.github.io/movies/", image:"./images_projects/img_movie.png", category:"webs"},
     // {id:3, description:"Esta aplicación permite llevar registro y control de citas, historias clinicas, asignación de medicamentos y recursos. Fué creada con la finalidad de obviar el papeleo y facilitar la actualización y mejor manejo de recursos. ", name:"Software HC", link:"ffhfhf", image:"./images/proyecto3.jpg", category:"app"},
     {id:4, description:"Esta pequeña aplicacion puede ser utilizada por plataformas para mostrar testimonios o comentarios de usuarios.", name:"Slider", link:"https://aura-baza.github.io/comments_slider/", image:"./images_projects/img_slider.png", category:"webs"},
     {id:5, description:"Muestra el incremento o decremento de un numero deacuerdo al boton que se le de click", name:"Counter", link:"https://aura-baza.github.io/counter/", image:"./images_projects/img_counter.png", category:"webs"},
-    {id:5, description:"Muestra colores de manera aleatoria", name:"Show Colors", link:"https://aura-baza.github.io/showing_colors/", image:"./images_projects/img_colors.png", category:"webs"},
+    {id:6, description:"Muestra colores de manera aleatoria", name:"Show Colors", link:"https://aura-baza.github.io/showing_colors/", image:"./images_projects/img_colors.png", category:"webs"},
+    {id:7, description:"Esta pequeña aplicacion muestra una ventanita al dar click al boton que corresponde mostrarla", name:"Show Modal", link:"https://aura-baza.github.io/modal/", image:"./images_projects/img_modal.png", category:"app"},
 ];
 
 
